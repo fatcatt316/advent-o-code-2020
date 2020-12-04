f06becf9fe842ac76bed48d4ea9eb1f20f053b0e
Joe Peck <fatcatt316@yahoo.com>
2020-12-03 21:20:37 -0500

    How's it going, Python?

--- day03/sled.py
+++ day03/sled.py
@@ -14,22 +14,14 @@ class Sled:
 
     while not sled.finished():
       sled.sled()
-
-    print(f"Slope: ({x_increment}, {y_increment}) -> Trees hit: {sled.trees_hit}")
+    return sled.trees_hit
 
   def sled(self):
-    self.set_next_x_pos()
-    self.y_pos = self.y_pos + self.y_increment
+    self.x_pos = (self.x_pos + self.x_increment) % len(self.tree_map[0])
+    self.y_pos += self.y_increment
 
     if self.hit_tree():
-      self.trees_hit = self.trees_hit + 1
-
-  def set_next_x_pos(self):
-    self.x_pos = self.x_pos + self.x_increment
-
-    # Adjust if it went off the right edge of the map
-    if (self.x_pos + 1) >= len(self.tree_map[0]):
-      self.x_pos = self.x_pos - len(self.tree_map[0])
+      self.trees_hit += 1
 
   def hit_tree(self):
     return self.tree_map[self.y_pos][self.x_pos] == "#"
@@ -47,17 +39,11 @@ class Sled:
         line = fp.readline().strip()
 
 ######## TESTS
-
 print("Test: this should be 7")
-Sled.run('./test_input.txt', 3, 1)
+print(Sled.run('./test_input.txt', 3, 1))
 
 print("Part 1:")
-Sled.run('./input.txt', 3, 1)
+print(Sled.run('./input.txt', 3, 1))
 
 print("Part 2:")
-print("Multiply these numbers together yourself!")
-Sled.run('./input.txt', 1, 1)
-Sled.run('./input.txt', 3, 1)
-Sled.run('./input.txt', 5, 1)
-Sled.run('./input.txt', 7, 1)
-Sled.run('./input.txt', 1, 2)
+print(Sled.run('./input.txt', 1, 1) * Sled.run('./input.txt', 3, 1) * Sled.run('./input.txt', 5, 1) * Sled.run('./input.txt', 7, 1) * Sled.run('./input.txt', 1, 2))
